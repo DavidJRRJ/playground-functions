560c6315d34fb5d11f3468fec6ebe659eae1868c
David Junior <david.lourenco94@gmail.com>
2022-04-12 20:23:10 -0300

    Função hydrate OK

--- src/challenges2.js
+++ src/challenges2.js
@@ -11,10 +11,19 @@ function triangleCheck(lineA, lineB, lineC) {
 }
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(agua) {
+  let regex = /\d+/g;
+  let num = agua.match(regex);
+  let soma = 0
+  let msg; 
+  for(let i = 0; i < num.length; i++){
+    soma += parseInt(num[i]);
+  }
+  if (soma === 1) msg = soma + ' ' + 'copo de água';
+  else msg = soma + ' ' + 'copos de água';
+  return(msg);
 }
-
+//console.log(hydrate("Beba 1 copo de cerveja e 3 vodcas"));
 module.exports = {
   generatePhoneNumber,
   hydrate,

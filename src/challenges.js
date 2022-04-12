f9900d4294c45f82fa2c3a0d5df5c501ef6bdab1
David Junior <david.lourenco94@gmail.com>
2022-04-12 17:30:49 -0300

    funcao catAndMouse ok

--- src/challenges.js
+++ src/challenges.js
@@ -36,13 +36,17 @@ function footballPoints(wins, ties) {
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(arrNum) { 
+  
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let dist1 = Math.abs(mouse - cat1);
+  let dist2 = Math.abs(mouse - cat2);
+  if (dist1 < dist2) return("cat1");
+  else if (dist2 < dist1) return("cat2");
+  else if (dist1 === dist2) return("os gatos trombam e o rato foge");
 }
 
 // Desafio 8
